259f3ddd5dbc5174c456f8b48fff7e42c14f5772
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-11-27 22:41:45 -0300

    feat(Api): Atualização do CORS para acesso do Blazor

--- Portfolio.Api/Program.cs
+++ Portfolio.Api/Program.cs
@@ -1,15 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor",
+        p => p.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowBlazor");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
