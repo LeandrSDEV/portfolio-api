3b308000814acb09a55b79cd022d3f55e59b3881
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-11-28 13:56:31 -0300

    feat(api): Link completo da api com o blazor

---
+++ Portfolio.Api/Controllers/ProjetosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portifolio.Domain.Entities;
+
+namespace Portfolio.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjetosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var lista = new List<Project>
+            {
+                new Project { Id = 1, Title = "Projeto Teste", Description = "Funcionando!" }
+            };
+
+            return Ok(lista);
+        }
+    }
+}
---
+++ Portfolio.Api/Controllers/TesteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Funcionando!");
+    }
+}
