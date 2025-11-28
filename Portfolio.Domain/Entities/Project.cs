8b1c2edafb7b1e7203e92d81bc30454c5c25218a
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-11-27 23:08:47 -0300

    chore(api): Atualização com vinculo ao Blazor

---
+++ Portfolio.Domain/Entities/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Portifolio.Domain.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> Tech { get; set; }
+        public string GithubUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
